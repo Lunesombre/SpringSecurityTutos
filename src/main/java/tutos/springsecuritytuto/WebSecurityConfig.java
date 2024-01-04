c88a38ac5a8346157fe774eabb95d08833b982c6
Romain Touchet <romain.touchet@inetum.com>
2024-01-04 17:30:02 +0100

    CLEVW-0000 - feat(tuto) : roles are now definable per profile, one user can have more than one role

--- src/main/java/tutos/springsecuritytuto/WebSecurityConfig.java
+++ src/main/java/tutos/springsecuritytuto/WebSecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configurers.LogoutConf
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,6 +22,8 @@ public class WebSecurityConfig {
     private String userPassword;
     @Value("${USER_NAME}")
     private String userName;
+    @Value("${USER_ROLES:}")
+    private String[] userRoles;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -41,18 +43,23 @@ public class WebSecurityConfig {
     // provides a default User
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user =
+        // J'ai fait cette modif pour pouvoir accepter plusieurs roles pour un user. User.builder().roles() attend un String[] : on lui en fournit un oupas, s'il n'en a pas ou bien un vide il attribue "USER", sachant que derrière, ça transforme les roles en Granted Authorities =[ROLE_NOMDUROLE]
+        User.UserBuilder userBuilder =
                 User.builder()
                         .username(userName)
                         .password(passwordEncoder().encode(userPassword))
-                        .roles("USER")
-                        .build();
+                        .roles((userRoles != null && userRoles.length > 0) ? userRoles : new String[]{"USER"});
+
+        UserDetails user = userBuilder.build();
         return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        // this one uses bcrypt
+//        return new BCryptPasswordEncoder();
+        // this one uses the current standard, whatever it is :
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
